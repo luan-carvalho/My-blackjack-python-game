b4db03dfe80fc8231b8dc83ae96bc1b5ffb89bbe
Luan Carvalho <ita.luan12@gmail.com>
2022-05-04 18:08:30 -0300

    Add game classes and game logic.

---
+++ classes.py
@@ -0,0 +1,59 @@
+values = {"Ace": 11, "Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6,
+          "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 10, "Queen": 10, "King": 10}
+
+suits = ("Cups", "Spades", "Diamonds", "Hearts")
+
+ranks = ("Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
+         "Eight", "Nine", "Ten", "Jack", "Queen", "King")
+
+
+class Card:
+
+    def __init__(self, rank, suit):
+
+        self.rank = rank
+        self.suit = suit
+        self.value = values[rank]
+        self.face_up = True
+
+    def __str__(self):
+
+        if self.face_up == True:
+
+            return f"{self.rank} of {self.suit}"
+
+        else:
+
+            return "This card is face-down"
+
+    def __repr__(self):
+
+        if self.face_up == True:
+
+            return f"{self.rank} of {self.suit}"
+
+        else:
+
+            return "This card is face-down"
+
+
+class Deck:
+
+    def __init__(self):
+
+        import random
+
+        self.all_cards = []
+
+        for suit in suits:
+
+            for rank in ranks:
+
+                self.all_cards.append(Card(rank, suit))
+
+        random.shuffle(self.all_cards)
+
+    def get_one_card(self):
+
+        my_card = self.all_cards.pop(0)
+        return my_card
---
+++ game.py
@@ -0,0 +1,13 @@
+# This is the game it self
+
+# Game logic
+
+# 1 Create the dealer and the player
+# 2 The  player bets and the dealer deals two cards for the player and for himself
+# 3 Check if the player has a blackjack hand (if yes, the game ends and the player wins 3:2)
+# 4 The player chooses his move(hit, stay, double-down or split)
+# 5 Check if the player busts
+# 6 Dealer reveals his card
+# 7 The dealer stays or hit
+# 8 Check the winner
+# 9 The player chooses to repeat or not
