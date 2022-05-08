d2ca653459bbecf018d0ab27078db5121af88750
Luan Carvalho <ita.luan12@gmail.com>
2022-05-07 23:34:09 -0300

    Add more game logic.

--- game.py
+++ game.py
@@ -15,9 +15,27 @@ dealer.deal_cards(player, 2)
 dealer.get_cards()
 
 # 3 Check if the player has a blackjack hand (if yes, the game ends and the player wins 3:2)
+
+dealer.check_blackjack(player)
+
 # 4 The player chooses his move(hit, stay, double-down or split)
-# 5 Check if the player busts
-# 6 Dealer reveals his card
-# 7 The dealer stays or hit
-# 8 Check the winner
-# 9 The player chooses to repeat or not  
+
+player.play(dealer)
+
+# 5 Dealer reveals his card
+
+dealer.reveal_card()
+
+# 7 Check the winner
+
+dealer.check_win(player)
+
+# 8 The player chooses to repeat or not 
+
+# if repeat(player, dealer):
+
+#     continue
+
+# else:
+
+#     break
