2d5b27addad067889c3a8bb5337b25b2a6cde070
Luan Carvalho <ita.luan12@gmail.com>
2022-05-08 22:05:09 -0300

    Update game logic.

--- game.py
+++ game.py
@@ -14,12 +14,21 @@ while player.has_money(min_bet):
     # 2 The  player bets and the dealer deals two cards for the player and for himself
 
     player.bet(min_bet)
-    dealer.deal_cards(player, 2)
+    # dealer.deal_cards(player, 2)
+    player.cards = [Card("Ace", "Spades"), Card("Ten", "Hearts")]
     dealer.get_cards()
 
     # 3 Check if the player has a blackjack hand (if yes, the game ends and the player wins 3:2)
 
-    dealer.check_blackjack(player)
+    if dealer.check_blackjack(player):
+
+        if repeat(player, dealer):
+
+            continue
+
+        else:
+
+            break
 
     # 4 The player chooses his move(hit, stay, double-down or split)
 
