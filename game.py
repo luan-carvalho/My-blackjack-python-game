2f0bc2e4471a31790125d8c6ca15f03c405446d4
Luan Carvalho <ita.luan12@gmail.com>
2022-05-09 15:12:11 -0300

    Add a screen break function and a delay between game actions.

--- game.py
+++ game.py
@@ -1,8 +1,22 @@
 # This is the game it self
 from classes import *
+from time import sleep
+
+
+def screen_break():
+
+    print("\n")
+    print("*"*100)
+    print("\n")
+
 # Game logic
 
 # 1 Create the dealer and the player
+
+screen_break()
+
+print("\nWelcome to the Monkey's cassino!! This is a Blackjack table. The minimal bet is $300.")
+print("\nYou have $1000")
 
 dealer = Dealer()
 player = Player()
@@ -13,13 +27,35 @@ while player.has_money(min_bet):
 
     # 2 The  player bets and the dealer deals two cards for the player and for himself
 
+    screen_break()
+
     player.bet(min_bet)
+
+    screen_break()
+
+    sleep(2)
+
+    print("\nDealing your cards...")
+
+    sleep(2)
+
     dealer.deal_cards(player, 2)
+
+    screen_break()
+    
+    sleep(2)
+
+    print("\nDealing dealer's cards...")
+
+    sleep(2)
+
     dealer.get_cards()
 
     # 3 Check if the player has a blackjack hand (if yes, the game ends and the player wins 3:2)
 
     if dealer.check_blackjack(player):
+
+        sleep(2)
 
         if repeat(player, dealer):
 
@@ -31,9 +67,15 @@ while player.has_money(min_bet):
 
     # 4 The player chooses his move(hit, stay, double-down or split)
 
+    screen_break()
+
+    sleep(2)
+
     player.play(dealer)
 
     if has_busted(player):
+
+        sleep(1)
 
         if repeat(player, dealer):
 
@@ -43,14 +85,23 @@ while player.has_money(min_bet):
 
             break
 
+    
     # 5 Dealer reveals his card
+
+    screen_break()
+
+    sleep(2)
 
     dealer.reveal_card()
 
     if has_busted(dealer):
 
+        sleep(2)
+
         player.money += player.round_bet*2
         print(f"\nPlayer money: {player.money}")
+
+        sleep(2)
 
         if repeat(player, dealer):
 
@@ -62,9 +113,17 @@ while player.has_money(min_bet):
 
     # 7 Check the winner
 
+    screen_break()
+
+    sleep(2)
+
     dealer.check_win(player)
 
     # 8 The player chooses to repeat or not
+
+    screen_break()
+
+    sleep(2)
 
     if repeat(player, dealer):
 
